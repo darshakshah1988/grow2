07516d2cbc961efe8c7f4a30401de225ed500441
darshakshah1988 <darshakshah1988@gmail.com>
2020-08-29 00:13:45 +0530

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
-export const BASE_URL: string = window.location.host === 'localhost:8080'
-    ? 'http://localhost:5000'
+export const BASE_URL: string = window.location.host === 'https://stagegrow.herokuapp.com/'
+    ? 'https://stagegrow.herokuapp.com/'
     : window.location.origin
 
 export const STRIPE_PUBLISHABLE_KEY = 'pk_test_51H7mptIVvM8zimDuPVOYQ4TwtQC5xQNj4ETWPU9PJZW7TcEDo6StW4rL7aPaJw8uhTDWAbATLpoP2wyQmWNG0yx900nTnc77V2'
